6a1cf3c08810bd8e0cead054bfd16ddcc42712f4
souzar2 <arthursouzacomp@gmail.com>
2023-08-28 14:37:25 -0300

    so alguns bugs consertados

--- testRec.py
+++ testRec.py
@@ -7,10 +7,11 @@ from PIL import Image
 
 def main():
     print("Se preferir, é possível digitar o caminho completo da imagem que será utilizada como teste...")
+    print()
     print("Se não, a foto utilizada como padrão do teste será a do Tony Stark")
 
     caminho_arquivo = input("Digite o caminho completo da imagem: ")
-    
+
     try:
         imgTest = fr.load_image_file(caminho_arquivo)
         # Faça o processamento necessário com a imagem aqui
@@ -21,9 +22,7 @@ def main():
 
     video_cap = cv2.VideoCapture(0)
     rostoCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    olhosCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-    sorrisoCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
-
+    cont = 0
     imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
     corBorda = [255, 0, 0]
@@ -40,10 +39,14 @@ def main():
             minSize=(30, 30)
         )
 
-        rostoEncondings = fr.face_encodings(rgb)[0]
-        encodeTest = fr.face_encodings(imgTest)[0]
-
-        comparacao = fr.compare_faces([rostoEncondings], encodeTest)[0]
+        if(cont%40==0):
+            comparacao = False
+                
+            rostoEncondings = fr.face_encodings(rgb)
+            encodeTest = fr.face_encodings(imgTest)
+            
+            if(len(rostoEncondings)>0 and len(encodeTest)>0):
+                comparacao = fr.compare_faces([rostoEncondings[0]], encodeTest[0])[0]
 
         if comparacao:
             corBorda = [0, 255, 0]
@@ -54,6 +57,8 @@ def main():
             cv2.rectangle(frame, (x, y), (x+w, y+h), (corBorda), 2)
 
         cv2.imshow('video', frame)
+
+        cont += 1
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
